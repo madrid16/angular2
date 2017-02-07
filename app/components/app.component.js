c47a89da566cafe9cb79b405f3ea0ef55f478c08
Madrid16 <m.madrid16@gmail.com>
2017-02-07 13:32:22 -0300

    give path for deploy test file

--- app/components/app.component.js
+++ app/components/app.component.js
@@ -1,5 +1,4 @@
 "use strict";
-<<<<<<< HEAD
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -31,6 +30,4 @@ var AppComponent = (function () {
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
-=======
->>>>>>> master
 //# sourceMappingURL=app.component.js.map
